e3c67216c7ffc612c19ce2cb961d1a9141b23603
Secc-React-Repo <142501678+Secc-React-Repo@users.noreply.github.com>
2023-08-23 09:57:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Form from "./Form";
 import Welcome from "./Welcome";
-import "./App.css"; // Import CSS for this component
+import "./App.css"; 
 
 const App = () => {
   const [userDetails, setUserDetails] = useState(null);
